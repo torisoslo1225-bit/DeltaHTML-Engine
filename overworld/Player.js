a9affd3a53c2b7eb1daac518d5f4865fefb03a3c
torisoslo1225-bit <torisoslo1225@gmail.com>
2026-08-05 15:04:27 -0300

    Part 2 upgrade player movement

--- overworld/Player.js
+++ overworld/Player.js
@@ -1,21 +1,21 @@
 import Input from "../engine/Input.js";
 
 export default class Player {
-    constructor(){
-        this.x = 320;
-        this.y = 240;
-        this.speed = 120;
-    }
-
-    update(dt){
-        if(Input.down("ArrowLeft")) this.x -= this.speed * dt;
-        if(Input.down("ArrowRight")) this.x += this.speed * dt;
-        if(Input.down("ArrowUp")) this.y -= this.speed * dt;
-        if(Input.down("ArrowDown")) this.y += this.speed * dt;
-    }
-
-    draw(ctx){
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.x,this.y,16,24);
-    }
+ constructor(){
+  this.x=320;
+  this.y=240;
+  this.speed=120;
+  this.size=16;
+  this.direction="down";
+ }
+ update(dt){
+  if(Input.down("ArrowLeft")){this.x-=this.speed*dt;this.direction="left";}
+  if(Input.down("ArrowRight")){this.x+=this.speed*dt;this.direction="right";}
+  if(Input.down("ArrowUp")){this.y-=this.speed*dt;this.direction="up";}
+  if(Input.down("ArrowDown")){this.y+=this.speed*dt;this.direction="down";}
+ }
+ draw(ctx){
+  ctx.fillStyle="red";
+  ctx.fillRect(this.x,this.y,this.size,24);
+ }
 }
