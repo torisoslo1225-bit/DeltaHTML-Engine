6b6aabbe55db55c803cad85095d1e34e7adb42d6
torisoslo1225-bit <torisoslo1225@gmail.com>
2026-08-05 15:19:48 -0300

    Connect test overworld to engine

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import Engine from "./engine/Engine.js";
+import TestRoom from "./overworld/TestRoom.js";
+import TestPlayer from "./overworld/TestPlayer.js";
 
 window.addEventListener("load", () => {
   try {
     const engine = new Engine();
+    engine.scene = new TestRoom();
+    engine.player = new TestPlayer(320,240);
     engine.start();
   } catch (error) {
     document.body.innerHTML = `<pre style="color:white;background:black;padding:20px;font-size:18px">DeltaHTML Engine Error:\n${error.stack}</pre>`;
