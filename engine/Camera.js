6884920cdc59f6e5064a288bec967b57d153c0eb
torisoslo1225-bit <torisoslo1225@gmail.com>
2026-08-05 16:05:07 -0300

    Connect camera follow system

--- engine/Camera.js
+++ engine/Camera.js
@@ -1,6 +1,17 @@
+// Simple Deltarune-style camera follow system
+
 export default class Camera {
- constructor(){
-  this.x=0;
-  this.y=0;
- }
+  constructor(){
+    this.x = 0;
+    this.y = 0;
+  }
+
+  follow(target, width, height){
+    this.x = target.x - width / 2;
+    this.y = target.y - height / 2;
+  }
+
+  apply(ctx){
+    ctx.translate(-this.x, -this.y);
+  }
 }
