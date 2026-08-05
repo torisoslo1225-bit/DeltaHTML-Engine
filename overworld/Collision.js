6d7a39ae33bcbc812cb600d3e772c766c86621e7
torisoslo1225-bit <torisoslo1225@gmail.com>
2026-08-05 15:03:19 -0300

    Add Part 2 collision system

---
+++ overworld/Collision.js
@@ -0,0 +1,8 @@
+export default class Collision {
+    static box(a,b){
+        return a.x < b.x+b.width &&
+        a.x+a.width > b.x &&
+        a.y < b.y+b.height &&
+        a.y+a.height > b.y;
+    }
+}
